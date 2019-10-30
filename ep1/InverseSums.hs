6c1b8433e936d0a2d9ad87566de40d0984d04f78
amanda <amanda@ptank.com>
2019-10-30 15:50:05 -0700

    inverse sums for first Haskell video

---
+++ ep1/InverseSums.hs
@@ -0,0 +1,30 @@
+import Prelude hiding (sum)
+
+
+sum :: Num a => Int -> [a] -> a
+sum n series = foldl (+) 0 (take n series)
+
+naturals = [1..]
+
+harmonics = [1/x    | x <- naturals]
+inverseSquares = [1/x**2 | x <- naturals]
+inverseCubes   = [1/x**3 | x <- naturals]
+
+harmonicSeries n = sum n harmonics
+inverseSquaresSeries n = sum n inverseSquares
+inverseCubesSeries n = sum n inverseCubes
+
+
+powerSeries n = [1/n**x  | x <- naturals]
+
+power2 = powerSeries 2
+power3 = powerSeries 3
+
+
+factorial :: (Num a, Eq a) => a -> a
+factorial 0 = 1
+factorial n = n * (factorial (n - 1))
+
+factorials = [1/(factorial x) | x <- 0:naturals]
+
+factorialSeries n = sum n factorials
